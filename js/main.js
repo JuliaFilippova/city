39007be02b3a837fa6bdb2e5ffa0eca3c4cd54ba
Julia Filippova <mega.yulia1996@mail.ru>
2020-02-23 22:06:57 +0300

    +

---
+++ js/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+$(document).ready(function () {
+    $('.slider-reviews').slick({
+        // centerMode:     true,
+        // centerPadding:  '1px',
+        dots:           false,
+        infinite:       true,
+        speed:          700,
+        slidesToShow:   3,
+        slidesToScroll: 1,
+        arrows:         true,
+        responsive:     [{
+            breakpoint: 1300,
+            settings:   {
+                slidesToShow:   2,
+                slidesToScroll: 1
+            }
+        },
+            {
+                breakpoint: 920,
+                settings:   {
+                    slidesToShow:   1,
+                    slidesToScroll: 1
+                }
+            }]
+    });
+});
